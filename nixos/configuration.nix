70cd2dcdda483b081c52d166c577feeb7eb8b310
Eric Sharma <sharma.e@husky.neu.edu>
2026-03-05 23:02:14 +0000

    feat: disable password login for ssh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,8 +26,12 @@
 
   # ── SSH ────────────────────────────────────────────────────────────────────
   services.openssh.enable                            = true;
-  services.openssh.settings.PermitRootLogin          = "yes";
-  services.openssh.settings.PasswordAuthentication   = true;
+  services.openssh.settings.PermitRootLogin          = "prohibit-password";
+  services.openssh.settings.PasswordAuthentication   = false;
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQ4z1+PkPXFCY7Ts9XJbchYdT/oGKpifwdWK/axxf2H eric@ericsharma.xyz"
+  ];
 
   # ── System packages ────────────────────────────────────────────────────────
   environment.systemPackages = with pkgs; [
