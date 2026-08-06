b98ef5ac34b9a70f14cfe99760d0c86d6a3f9518
Eric Sharma <sharma.e@husky.neu.edu>
2026-08-06 10:47:35 -0400

    fix: bind cadvisor to all interfaces so remote hosts can be scraped

--- hosts/nixos/optional/monitoring/exporters.nix
+++ hosts/nixos/optional/monitoring/exporters.nix
@@ -17,6 +17,10 @@
   services.cadvisor = {
     enable = true;
     port = 9101;
+    # The NixOS default is 127.0.0.1, which only works when Prometheus scrapes
+    # the host as 127.0.0.1 (trigkey). A host reached by LAN address needs a
+    # wildcard bind, matching node_exporter's default and the firewall rule below.
+    listenAddress = "0.0.0.0";
   };
 
   networking.firewall.allowedTCPPorts = [
